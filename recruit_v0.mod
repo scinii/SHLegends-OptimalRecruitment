a72814b0f60f15cb75af56f06ff7e51f9062b780
scinii <roberto.schinina02@gmail.com>
2025-04-24 22:45:57 +0200

    v0 done

--- recruit_v0.mod
+++ recruit_v0.mod
@@ -17,6 +17,15 @@ param hon_cost{U}; # cost of the unit in honour
 param avaliable_gold;
 param avaliable_honour;
 param avaliable_people;
+param avaliable_bow;
+param avaliable_crossbow;
+param avaliable_spear;
+param avaliable_mace;
+param avaliable_pike;
+param avaliable_sword;
+param avaiable_leather;
+param avaliable_plate;
+
 
 # OBJECTIVE FUNCTION
 
@@ -29,3 +38,15 @@ minimize objective_function: sum{u in U}x[u]*(vuln[u] - health[u] - damage[u] - 
 subject to capital_constraints: sum{u in U} x[u] * gold_cost[u] <= avaliable_gold;
 subject to honour_constraint: sum{u in U} x[u] * hon_cost[u] <= avaliable_honour;
 subject to people_constraint: sum{u in U} x[u] <= avaliable_people;
+
+
+# New-Stuff
+
+subject to bow_constraint: x['Archer'] <= avaliable_bow;
+subject to crossbow_constraint: x['Crossbowman'] <= avaliable_crossbow;
+subject to spearman_constraint: x['Spearman'] <= avaliable_spear;
+subject to pikeman_constrain: x['Pikeman'] <= avaliable_pike;
+subject to maceman_constraint: x['Maceman'] <= avaliable_mace;
+subject to sword_constraint: x['Swordsman'] + x['Knight'] <= avaliable_sword;
+subject to leather_constraint: x['Crossbowman'] + x['Maceman'] <= avaiable_leather;
+subject to plate_constraint: x['Swordsman'] + x['Knight'] + x['Pikeman'] <= avaliable_plate;
--- recruit_v0.py
+++ recruit_v0.py
@@ -2,12 +2,10 @@ import numpy as np
 import pandas as pd
 from amplpy import AMPL
 
-# v0: given gold and honour. Calculate the optimal number of troops.
-# v1: v0 +  you are also given armour conditions.
-# v2: v1 + you can buy and sell armour.
-# v3: v2 + you can buy and sell other resources
-# v4: v3 + you schedule the people working as well
-# sh_troops_recruitment_v0
+# v0: given gold, honour and people , armour. Calculate the optimal number of troops ---- DONE ----
+# v1: you can buy and sell armour.
+# v3: v2 + you can sell other resources
+
 """
     If you are not using amplpy.modules (e.g. highs, coin), and the AMPL installation directory
     is not in the system search path, add it as follows:  
@@ -15,28 +13,48 @@ from amplpy import AMPL
     add_to_path(r"full path to the AMPL installation directory")
 """
 
-def get_optimal_troops(avaliable_gold, avaliable_honour, avaliable_people):
+def get_optimal_troops(avaliable_gold, avaliable_honour, avaliable_people, avaliable_bow,
+                       avaliable_crossbow, avaliable_spear, avaliable_mace, avaliable_pike, avaliable_sword, avaiable_leather, avaliable_plate):
 
     troops_stats = pd.read_csv("sh_legends_troops_stats.csv")
+
     troops = AMPL()
     troops.read("recruit_v0.mod")
     troops.option["solver"] = "highs"
 
+
+    health = troops_stats['Hit points'].values
+    damage = troops_stats['Damage'].values
+    avg_vuln = (troops_stats['Miss Arm'] + troops_stats['Blade Arm'] + troops_stats['Imp Arm']).values / 3
+    eff_vuln = avg_vuln.sum() / health
+    agility = troops_stats['Run'].values
+
     troops.set["U"] = troops_stats['Type'].values
-    troops.param["health"] = (1/11500) * troops_stats['Hit points'].values
-    troops.param["damage"] = (1/38) * troops_stats['Damage'].values
-    troops.param["vuln"] = ((1/0.53) * troops_stats['Miss Arm'] + (1/0.6) *troops_stats['Blade Arm'] + (1/0.6) *troops_stats['Imp Arm']).values
-    troops.param["agility"] = (1/6) *  troops_stats['Run'].values
+    troops.param["health"] = health / health.mean()
+    troops.param["damage"] = damage / damage.mean()
+    troops.param["vuln"] = eff_vuln
+    troops.param["agility"] = agility / agility.mean()
     troops.param["gold_cost"] = troops_stats['Gold Cost'].values
     troops.param["hon_cost"] = troops_stats['Honour Cost'].values
+
+
     troops.param["avaliable_gold"] = avaliable_gold
     troops.param["avaliable_honour"] = avaliable_honour
     troops.param["avaliable_people"] = avaliable_people
+    troops.param["avaliable_bow"] = avaliable_bow
+    troops.param["avaliable_crossbow"] = avaliable_crossbow
+    troops.param["avaliable_spear"] = avaliable_spear
+    troops.param["avaliable_mace"] = avaliable_mace
+    troops.param["avaliable_pike"] = avaliable_pike
+    troops.param["avaliable_sword"] = avaliable_sword
+    troops.param["avaiable_leather"] = avaiable_leather
+    troops.param["avaliable_plate"] = avaliable_plate
 
     troops.solve()
 
-    return troops
+    return troops.var["x"].to_dict()
 
-t = get_optimal_troops(1000,10000,10)
+t = get_optimal_troops(10000,10000,50,
+                       5,0,0,0,0,0,0,0)
 
-print( t.var["x"].to_dict() )
+print(t)
